53e09eae42779cb8a25987eee1a9e5833d8f4daf
Anthony Wijnen <anthony.wijnen@gmail.com>
2013-04-07 17:42:28 -0400

    conditional in facebook helper

--- app/helpers/facebook_helper.rb
+++ app/helpers/facebook_helper.rb
@@ -1,7 +1,7 @@
 module FacebookHelper
 
   def facebook_pic
-     current_user.identities.where(:provider => 'facebook').first.image
+     current_user.identities.where(:provider => 'facebook').first.image if current_user.has_identity?
   end
 
 end
