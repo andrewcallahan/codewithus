5d3d3df9a88d39b9d2f97cd11b46f9f0bc5999d1
Anthony Wijnen <anthony.wijnen@gmail.com>
2013-04-06 22:11:40 -0400

    edit seed

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130407013952) do
+ActiveRecord::Schema.define(:version => 20130407020548) do
 
   create_table "hackathons", :force => true do |t|
     t.string   "name"
@@ -79,13 +79,12 @@ ActiveRecord::Schema.define(:version => 20130407013952) do
     t.string   "name"
     t.string   "email"
     t.string   "password_digest"
-    t.string   "password_confirmation"
     t.string   "cell"
     t.string   "image"
     t.string   "image_file_name"
     t.string   "image_content_type"
-    t.datetime "created_at",            :null => false
-    t.datetime "updated_at",            :null => false
+    t.datetime "created_at",         :null => false
+    t.datetime "updated_at",         :null => false
   end
 
 end
