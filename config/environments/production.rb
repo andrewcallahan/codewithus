361608637330c54f1c44e3e5a3013845b8db43dd
Danny Olinsky <dannyolinsky@gmail.com>
2013-04-23 10:45:13 -0400

    compile on runtime

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -20,6 +20,8 @@ Codewithus::Application.configure do
   # Generate digests for assets URLs
   config.assets.digest = true
 
+  config.assets.compile = true
+
   # Defaults to nil and saved in location specified by config.assets.prefix
   # config.assets.manifest = YOUR_PATH
 
