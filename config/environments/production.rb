fef76787150beb46ee47e7709df1574418315cad
Danny Olinsky <dannyolinsky@gmail.com>
2013-04-23 10:59:23 -0400

    quick fix

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -15,7 +15,6 @@ Codewithus::Application.configure do
   config.assets.compress = true
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
-  config.assets.compile = false
 
   # Generate digests for assets URLs
   config.assets.digest = true
